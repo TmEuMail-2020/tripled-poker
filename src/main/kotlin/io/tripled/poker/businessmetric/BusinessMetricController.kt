06f3f955be5303108f7a4fc9c1d779503fcf75a5
khofmans <kris@rotateit.be>
2019-01-18 11:02:21 +0100

    Enable technical and business monitoring

---
+++ src/main/kotlin/io/tripled/poker/businessmetric/BusinessMetricController.kt
@@ -0,0 +1,17 @@
+package io.tripled.poker.businessmetric
+
+import io.micrometer.core.annotation.Timed
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class BusinessMetricController(registry: MeterRegistry) {
+    val counter= registry.counter("business.metric")
+
+    @GetMapping("/api/businessMetric")
+    @Timed
+    fun incrementCounter() {
+        counter.increment()
+    }
+}
