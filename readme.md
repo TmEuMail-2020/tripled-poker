f9b82d44e195a7ac388ef13e378c2300c9778ce7
khofmans <kris@rotateit.be>
2019-01-18 10:17:27 +0100

    Open tripled day poker project boilerplate

---
+++ readme.md
@@ -0,0 +1,8 @@
+## Initial setup
+
+https://start.spring.io
+* gradle, kotlin, spring-boot
+* web, embedded mongo, actuator
+
+https://github.com/graphql-java-kickstart/graphql-spring-boot
+* graphql & graphiql
---
+++ src/main/kotlin/io/tripled/poker/PokerApplication.kt
@@ -0,0 +1,12 @@
+package io.tripled.poker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PokerApplication
+
+fun main(args: Array<String>) {
+	runApplication<PokerApplication>(*args)
+}
+
