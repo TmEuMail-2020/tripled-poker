4890b08e843d443e4b638b4aa53664a18cbe5fb5
Kris Hofmans <kris@rotateit.be>
2019-02-09 20:28:56 +0000

    work on creating 1 module, called main, and extract further from there

--- infra-graphql/src/main/kotlin/io/tripled/poker/graphql/exceptions/ValidationException.kt
+++ infra-graphql/src/main/kotlin/io/tripled/poker/graphql/exceptions/ValidationException.kt
@@ -7,7 +7,7 @@ class ValidationException(val constraintErrors: List<ConstraintError>) : Runtime
 data class ConstraintError(val path: String, val message: String, val type: String)
 
 fun ConstraintViolation<*>.asConstraintError() = ConstraintError(
-    path = this.propertyPath.toString(),
-    message = this.message,
-    type = this.leafBean.toString()
+        path = this.propertyPath.toString(),
+        message = this.message,
+        type = this.leafBean.toString()
 )
