750e5caf70810f67332fba29bb3b84b28ecb2b61
khofmans <kris@rotateit.be>
2019-02-11 12:56:00 +0100

    CQRS!

--- app/src/main/kotlin/io/tripled/poker/api/TableService.kt
+++ app/src/main/kotlin/io/tripled/poker/api/TableService.kt
@@ -1,23 +1,15 @@
 package io.tripled.poker.api
 
-import com.sun.xml.internal.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table
-import io.tripled.poker.api.response.Player
-import io.tripled.poker.domain.PlayerJoinedTable
 import io.tripled.poker.domain.Table
 import io.tripled.poker.eventsourcing.EventStore
+import io.tripled.poker.projection.TableProjection
 
 class TableService(private val eventStore: EventStore) {
 
     fun join(name: String) {
-        val table = Table(eventStore.findById(1))
-        eventStore.save(1, table.join(name))
+        eventStore.save(1, Table().join(name))
     }
 
-    fun getTable(): io.tripled.poker.api.response.Table {
-        val table = Table(eventStore.findById(1))
-
-        val players = table.players.map { player -> Player(player.name) }.toList()
-        return io.tripled.poker.api.response.Table(players);
-    }
+    fun getTable() = TableProjection(eventStore.findById(1)).table
 
 }
--- app/src/main/kotlin/io/tripled/poker/domain/Table.kt
+++ app/src/main/kotlin/io/tripled/poker/domain/Table.kt
@@ -1,23 +1,11 @@
 package io.tripled.poker.domain
 
-class Player(val name: String)
-
 data class PlayerJoinedTable(val name: String)
 
-class Table(private val events: List<Any>) {
-    val players = mutableListOf<Player>()
-
-    init {
-        events.forEach { event ->
-            when (event) {
-                is PlayerJoinedTable -> players.add(Player(event.name))
-            }
-        }
-    }
+class Table {
 
     fun join(name: String): PlayerJoinedTable {
         return PlayerJoinedTable(name)
     }
-
 
 }
