97ea694ac1015c7740790d9c1731ff99a318c76f
khofmans <kris@rotateit.be>
2019-02-11 10:00:52 +0100

    test for app module

---
+++ app/src/test/kotlin/io/tripled/poker/PokerApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.tripled.poker
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class PokerApplicationTests {
+
+    @Test
+    internal fun `lets just see that regular tests work too`() {
+        assertEquals(1, 1)
+    }
+
+}
