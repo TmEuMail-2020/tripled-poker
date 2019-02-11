4117c7426f98b0c85e5b922a9f5a2e0cec3a741f
khofmans <kris@rotateit.be>
2019-02-11 16:05:35 +0100

    small refactor

--- app/src/main/kotlin/io/tripled/poker/domain/ShuffledDeck.kt
+++ app/src/main/kotlin/io/tripled/poker/domain/ShuffledDeck.kt
@@ -1,14 +1,13 @@
 package io.tripled.poker.domain
 
 class ShuffledDeck : Deck {
-    private val cards = mutableListOf<Card>()
+    private val cards = Suit.values().flatMap {  suit ->
+        Value.values().map { value ->
+            Card(suit, value)
+        }
+    }.toMutableList()
 
     init {
-        Suit.values().forEach { suit ->
-            Value.values().forEach { value ->
-                cards += Card(suit, value)
-            }
-        }
         cards.shuffle()
     }
 
