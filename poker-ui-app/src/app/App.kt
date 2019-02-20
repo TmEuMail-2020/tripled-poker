f141bfcdeb4cf2a182c2be5ac21658e3ab933759
khofmans <kris@rotateit.be>
2019-02-20 21:27:03 +0100

    working graphql result

--- poker-ui-app/src/app/App.kt
+++ poker-ui-app/src/app/App.kt
@@ -46,10 +46,10 @@ class AxiosExample : RComponent<RProps, RState>() {
 
         axios<dynamic>(config).then { response ->
             setState {
-                console.log(response.data)
+                //console.log(response.data)
                 persons = response.data
             }
-            console.log(response)
+            //console.log(response)
         }
     }
 
@@ -57,7 +57,7 @@ class AxiosExample : RComponent<RProps, RState>() {
         h1 { + "testing" }
 
         ul {
-            console.log(persons)
+            //console.log(persons)
             persons.map { person -> li { + person.name } }
         }
     }
@@ -69,13 +69,14 @@ enum class Suit { DIAMOND, SPADES, HEART, CLUB }
 enum class Value { TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE }
 data class Card(val value: Value, val suit: Suit)
 data class Player(val name: String, val cards: List<Card> = listOf())
-data class Table(val players: List<Player> = emptyList(), val winner: Player? = null)
-data class GraphqlResponse(val table: Table = Table())
+data class Table(val players: Array<Player> = emptyArray(), val winner: Player? = null)
+data class TableData(val table: Table)
+data class GraphqlResponse(val data: TableData)
 
 fun RBuilder.pokerTable() = child(PokerTableRepresentation::class){}
 
 class PokerTableRepresentation : RComponent<RProps, RState>() {
-    private var table: Table = Table(emptyList())
+    private var table: Table = Table()
 
     private fun createHeaders(): dynamic {
         val postHeaders = js("({})")
@@ -83,7 +84,7 @@ class PokerTableRepresentation : RComponent<RProps, RState>() {
         postHeaders["Content-Type"] = "application/json"
         postHeaders["test"] = "this123"
 
-        console.log(postHeaders)
+        //console.log(postHeaders)
 
         return postHeaders
     }
@@ -107,8 +108,8 @@ class PokerTableRepresentation : RComponent<RProps, RState>() {
 
         axios<GraphqlResponse>(config).then { response ->
             setState {
-                console.log(response.data)
-                //table = response.data.table
+                console.log(response.data.data.table)
+                table = response.data.data.table
             }
             console.log(response)
         }
