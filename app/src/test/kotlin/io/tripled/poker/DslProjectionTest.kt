4e6a25dac1a2cbe43b3a41108907fe9f73592990
khofmans <kris@rotateit.be>
2019-09-03 09:25:12 +0200

    explain why current dsl methods are coupled and how it can be fixed, remove all the boilerplate that's not needed in the test

--- app/src/test/kotlin/io/tripled/poker/DslProjectionTest.kt
+++ app/src/test/kotlin/io/tripled/poker/DslProjectionTest.kt
@@ -2,33 +2,23 @@ package io.tripled.poker
 
 import ch.tutteli.atrium.api.cc.en_GB.toBe
 import ch.tutteli.atrium.verbs.expect
-import io.tripled.poker.api.response.Suit
-import io.tripled.poker.api.response.Value
 import io.tripled.poker.domain.DeckMother
-import io.tripled.poker.domain.of
 import io.tripled.poker.dsl.DummyEventStore
-import io.tripled.poker.dsl.pokerGameTest
 import io.tripled.poker.dsl.pokerTableTest
 import io.tripled.poker.projection.DslProjection
 import org.junit.jupiter.api.Test
 
 class DslProjectionTest {
-    private val Joe = "Joe"
-    private val Jef = "Jef"
 
     @Test
     internal fun `test to see if we can generated the dsl out of the event stream`() = pokerTableTest {
         given {
             withCards(DeckMother().deckOfHearts())
-            withPlayers(Joe, Jef)
-            preflop(
-                    Joe to ((Value.TEN of Suit.HEARTS) and (Value.ACE of Suit.HEARTS)),
-                    Jef to ((Value.KING of Suit.HEARTS) and (Value.QUEEN of Suit.HEARTS))
-            ) {
-                Joe.checks()
-                Jef.checks()
-            }
+            withPlayers() /* todo fixme */
+            preflop {} /* todo fixme */
         }
+
+        // the fix would be that each call in the dsl fires at least 1 event to use in the projection and doesn't just collect state like with cards does now
 
         val dsl = DslProjection().dsl(DummyEventStore(this.eventStore.given.toMutableList() /* todo fixme */))
 
